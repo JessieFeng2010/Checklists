7002413e7d3980f2e45dbfd2f5a7c145791807f6
Jessie <jessie_feng2010@hotmail.com>
2015-01-09 14:20:41 +0800

    putting row data into the cells
    label in prototype cell
    @IBOutlet
    could only refer to the label from one of these cells, not all of them

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,23 +9,47 @@
 import UIKit
 
 class checklistViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell
+        
+        let label = cell.viewWithTag(1000) as UILabel
+        
+        switch(indexPath.row)
+        {
+        case 0:
+            label.text = "Walk the dog"
+            break
+        case 1:
+            label.text = "Brush my teeth"
+            break
+        case 2:
+            label.text = "Learn IOS development"
+            break
+        case 3:
+            label.text = "Soccer practice"
+            break
+        case 4:
+            label.text = "Eat ice cream"
+            break
+        default:
+            break
+        }
+        
         return cell
     }
 }
