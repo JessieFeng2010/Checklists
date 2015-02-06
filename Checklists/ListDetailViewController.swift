1682b48c76e2b4d6edb3860b6e53c9226be01fa8
Jessie <jessie_feng2010@hotmail.com>
2015-02-06 14:05:24 +0800

    new init()

--- Checklists/ListDetailViewController.swift
+++ Checklists/ListDetailViewController.swift
@@ -54,8 +54,7 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate, Icon
             delegate?.listDetailViewController(self, didFinishEditingChecklist: checklist)
         }
         else {
-            let checklist = Checklist(name: textField.text)
-            checklist.iconName = iconName
+            let checklist = Checklist(name: textField.text, iconName: iconName)
             delegate?.listDetailViewController(self, didFinishAddingChecklist: checklist)
         }
     }
